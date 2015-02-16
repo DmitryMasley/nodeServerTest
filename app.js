5aa7684680faf73082b5d4a304355834a3125e4f
DmitryMasley <ishuhard@gmail.com>
2015-02-16 17:42:49 +0200

    less support

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 i18n.expressBind(app, {

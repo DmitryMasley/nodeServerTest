e0fcf54194a7b1ce73f0b782250e9e604dd64aaa
DmitryMasley <ishuhard@gmail.com>
2015-02-16 23:12:13 +0200

    ru localization

--- locales/ru.js
+++ locales/ru.js
@@ -1,10 +1,10 @@
 {
-	"Username is required": "Username is required",
-	"Password is required": "Password is required",
-	"Role is required": "Role is required",
-	"Project Type is required": "Project Type is required",
-	"At least one language should be chosen": "At least one language should be chosen",
-	"Email is required": "Email is required",
-	"Email does not appear to be valid": "Email does not appear to be valid",
-	"Should accept terms and conditions": "Should accept terms and conditions"
+	"Username is required": "Требуется имя пользователя",
+	"Password is required": "Требуется пароль",
+	"Role is required": "Роль не выбрана",
+	"Project Type is required": "Укажите тип проекта",
+	"At least one language should be chosen": "Выбирете хотябы один язык",
+	"Email is required": "Укажите свой email",
+	"Email does not appear to be valid": "Email не корректный",
+	"Should accept terms and conditions": "Вы должны принять условия испльзования прежде чем продолжить"
 }

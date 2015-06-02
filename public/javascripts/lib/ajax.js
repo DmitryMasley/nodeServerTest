3f7de024be8e959e8e5017c977a9eaa937617f2f
TymchenkoOleksandr <tymchenko.oleksandr.87@gmail.com>
2015-06-02 14:31:09 +0300

    ajax

--- public/javascripts/lib/ajax.js
+++ public/javascripts/lib/ajax.js
@@ -1,7 +1,22 @@
-define([], function(){
+define([], function() {
     "use strict";
-    var ajax = function(config){
-
+    var ajax = function (method, url, responseSuccess, responseError, responseComplete) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp.status == 200) {
+                    responseSuccess(xmlhttp.responseText);
+                }
+                else if (xmlhttp.status == 400) {
+                    responseError(xmlhttp.status);
+                }
+                else {
+                    responseComplete(xmlhttp.status, xmlhttp.responseText)
+                }
+            }
+        };
+        xmlhttp.open(method, url, true);
+        xmlhttp.send();
     };
     return ajax;
-});
+}

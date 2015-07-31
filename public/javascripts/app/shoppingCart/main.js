fb33a02e568012f08e0282f5f5c50ceaf25469d9
TymchenkoOleksandr <tymchenko.oleksandr.87@gmail.com>
2015-07-31 13:27:07 +0300

    relative url for server use

--- public/javascripts/app/shoppingCart/main.js
+++ public/javascripts/app/shoppingCart/main.js
@@ -3,10 +3,10 @@ define(["marionette", "underscore", "./views/mainLayout"], function(Marionette, 
     var ShoppingCartMain = Marionette.Controller.extend({
         initialize: function(config){
             this.collection = new Backbone.Collection([
-                {id:1, title:"Crocs Classic 1", price:10, img:"images/crocs.jpg"},
-                {id:2, title:"Crocs Classic 2", price:21, img:"images/crocs.jpg"},
-                {id:3, title:"Crocs Classic 3", price:12, img:"images/crocs.jpg"},
-                {id:4, title:"Crocs Classic 4", price:33.3, img:"images/crocs.jpg"}
+                {id:1, title:"Crocs Classic 1", price:10, img:"/images/crocs.jpg"},
+                {id:2, title:"Crocs Classic 2", price:21, img:"/images/crocs.jpg"},
+                {id:3, title:"Crocs Classic 3", price:12, img:"/images/crocs.jpg"},
+                {id:4, title:"Crocs Classic 4", price:33.3, img:"/images/crocs.jpg"}
             ]);
             this.view = new LayoutView({el: config.el ? config.el : "body" , collection:this.collection});
             this.view.render();
